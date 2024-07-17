c6352864a688c3aebdd84ff2da69b77d6995dc2a
Charlotte Cronjaeger <92743481+charlottecrnj@users.noreply.github.com>
2024-07-17 13:17:41 +0200

    Make main.py clearer

--- main.py
+++ main.py
@@ -5,23 +5,20 @@ import os
 load_dotenv()
 
 def main():
-    # take user input of path of documents
-    folder_path = os.getenv('FOLDER_PATH_DOCUMENTS')
-
-    # load documents
-    data = loadEmbeddings(folder_path)
+    # load PDFs from specified folder
+    data = loadEmbeddings(os.getenv('FOLDER_PATH_DOCUMENTS'))
 
     # take user input of prompt
-    #query = "Can you give me information about the description of the warning symbols?"
+    # query = "Can you give me information about the description of the warning symbols?"
     query = input("Whats your question? ")
 
     # run RAG
     response = RAG(query, data)
 
+    # Display the results in the terminal 
     print(query,"\n\n")
     print(response)
 
 
 if __name__ == "__main__":
-
     main()
